29dea0385d816c62ae0f3beebf43f0804accdee7
Saruul <saruul-erdene@tussolution.mn>
2022-06-06 17:37:20 +0800

    second

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status, viewsets
 
 from profiles_api import serializers
+from profiles_api import models
 
 
 # Create your views here.
@@ -57,7 +58,6 @@ class HelloViewSet(viewsets.ViewSet):
     """"Test API ViewSet"""
     serializer_class = serializers.HelloSerializer
 
-
     def list(self, request):
         """"Return a hello message"""
 
@@ -98,3 +98,9 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """"Handle removing an object"""
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """"Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
