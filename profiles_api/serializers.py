0de9d5be623489659f5c4de043e9e44b00fc2f92
Saruul <saruul-erdene@tussolution.mn>
2022-06-06 17:49:05 +0800

    second

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 from profiles_api import models
 
 
-class HelloSerializer(serializers.Serializer, ABC):
+class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing out APIView"""
 
     name = serializers.CharField(max_length=10)
