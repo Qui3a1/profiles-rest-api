56f667d0033daf6ee7aba3f48f6003b3d45c7892
Saruul <saruul-erdene@tussolution.mn>
2022-06-06 18:33:08 +0800

    second

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -119,3 +119,13 @@ class UserLoginApiView(ObtainAuthToken):
     """"Handle creating user authentication tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication, )
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logger in user"""
+        serializer.save(user_profile=self.request.user)
